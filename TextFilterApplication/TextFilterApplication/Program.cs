770d1381d27d8b9bbf6f493ec8c3fa36f8feb342
Joseph Hodgson <joehodgson0@gmail.com>
2022-06-29 10:53:18 +0100

    Ran filters against sample text in console app

--- TextFilterApplication/TextFilterApplication/Program.cs
+++ TextFilterApplication/TextFilterApplication/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Services.Filters;
+using Services.Readers;
 
 namespace TextFilterApplication
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This program will apply 3 filters to the text in the file");
+
+            string fileName = "SampleText.txt";
+
+            var textFromFileReader = new TextFromFileReader();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceNamespace = $"{typeof(Program).Namespace}";
+
+            var textFromFile = await textFromFileReader.GetStringFromEmbeddedResourceTxtFile(assembly, resourceNamespace, fileName);
+
+            var filters = new List<IFilter>()
+            {
+                new WordsWithMiddleVowelFilter(),
+                new LengthLessThanThreeFilter(),
+                new WordsWithTFilter(),
+            };
+
+            var textFilterService = new TextFilterService(filters);
+
+            string resultAfterFilters = textFilterService.ApplyFilters(textFromFile);
+
+            Console.WriteLine($"Result: {resultAfterFilters}");
+
         }
     }
 }
